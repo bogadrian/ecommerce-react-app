35e0f534e9b83543b9b809ea984e1f32ec72bc26
Bogdan Adrian <bogdan4adrian@gmail.com>
2019-11-28 14:57:58 +0100

    removing home from package.json to try hewroku deplyment

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
-const middlwares = [logger];
+const middlwares = [];
+
+if (process.env.NODE_ENV !== 'development') {
+  middlwares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlwares));
 

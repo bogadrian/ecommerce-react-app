ce78febce4277e10a261dfb6f779e2ccee12c3bf
Bogdan Adrian <bogdan4adrian@gmail.com>
2019-12-11 14:11:14 +0100

    adding code for pwa

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -17,3 +18,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
--- src/pages/homepage/homepage.componenet.jsx
+++ src/pages/homepage/homepage.componenet.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import './homepage.style.scss';
 import Directory from '../../componenets/directory/directory.component';
 
 const HomePage = () => {
   return (
     <div className="homepage">
-      <Directory />
+      <Profiler
+        id="Directory"
+        onRender={(id, phase, actualDuration) => {
+          console.log(id, phase, actualDuration);
+        }}
+      >
+        <Directory />
+      </Profiler>
     </div>
   );
 };
